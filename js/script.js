b53df118af5babfe3d63674ceb27d0c070e5d15a
Agutembergues Silvares <gutemsilvares@hotmail.com>
2023-03-06 21:44:14 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const headerEl = document.querySelector('.bg-dark');
+
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > 10) {
+//     headerEl.classList.remove('bg-dark');
+//   } else {
+//     headerEl.classList.add('bg-dark');
+//   }
+// });
